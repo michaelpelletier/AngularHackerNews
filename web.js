c7fc3397f7b853ec4a3ba6787d852280ce58c013
Michael Pelletier <michael@mpelletier.net>
2016-11-13 19:29:37 -0500

    More testing.

--- web.js
+++ web.js
@@ -1,7 +1,15 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var port = process.env.PORT || 8000;
+
+// app.use(express.static(path.join(__dirname)));
 
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/build"));
-app.listen(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 5000);
+
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
